778210ddf11ab62543ea073935adecdaeb8486cd
braaist <ruslan0gumerov@gmail.com>
2025-01-20 13:04:17 +0800

    Volcano plots modified

--- volcano_plots.R
+++ volcano_plots.R
@@ -1,45 +1,53 @@
-library("apeglm")
-library("De")
-library("EnhancedVolcano")
+library(apeglm)
+library(EnhancedVolcano)
+library(DESeq2)
+library(tidyverse)
+library(tximport)
+library(ggsci)
+library(ggvenn)
 
-dds_use <- dds[,coldata_17p_only$samples]
-dds_use <- DESeq(dds_use)
-dds_result = results(dds_use)
+setwd("/Users/rusiq/Desktop/IGR/")
+source(paste0(getwd(),"/CustomFunctions.R"))
 
-resLFC <- lfcShrink(dds = dds_use, 
-                    res = dds_result,
-                    type = "normal",
-                    coef = "control_1_vs_0") 
+txi_3p_res = read.csv("/Users/rusiq/Desktop/IGR/txi_3p_res_output_initial.csv")
+txi_9p_res = read.csv("/Users/rusiq/Desktop/IGR/txi_9p_res_output_initial.csv")
+txi_17p_res = read.csv("/Users/rusiq/Desktop/IGR/txi_17p_res_output_initial.csv")
 
-??lfcShrink
-
-resLFC = na.omit(resLFC)
-resLFC = merge(data.frame(resLFC), gene_name, by = "row.names", all.x = TRUE)
-resLFC$log2FoldChange = -resLFC$log2FoldChange
-
-write.csv(resLFC, "/Users/rusiq/Desktop/IGR/DEG_17p_only_shranked.csv")
-
-
-EnhancedVolcano(toptable = resLFC,              
+EnhancedVolcano(toptable = txi_3p_res,
                 x = "log2FoldChange",
                 y = "padj",
-                lab = resLFC$gene_name
-)
-
-results_9p = read.csv("/Users/rusiq/Desktop/IGR/DEG_9p_only_shranked.csv")
-
-EnhancedVolcano(toptable = results_9p,
-                x = "log2FoldChange",
-                y = "padj",
-                lab = results_9p$gene_name,
+                lab = txi_3p_res$gene_name,
                 xlim = c(-10, +10),
                 ylim = c(0,100),
-                pCutoff = 0.001,
-                FCcutoff = 2.5, 
+                pCutoff = 0.05,
+                FCcutoff = 2, 
                 title = "del_3p vs. control samples"
 )
 
-results_9p[results_9p$padj < 0.001 & abs(results_9p$log2FoldChange) > 2.5,]
+EnhancedVolcano(toptable = txi_9p_res,
+                x = "log2FoldChange",
+                y = "padj",
+                lab = txi_9p_res$gene_name,
+                xlim = c(-10, +10),
+                ylim = c(0,100),
+                pCutoff = 0.05,
+                FCcutoff = 2, 
+                title = "del_9p vs. control samples"
+)
+
+EnhancedVolcano(toptable = txi_17p_res,
+                x = "log2FoldChange",
+                y = "padj",
+                lab = txi_17p_res$gene_name,
+                xlim = c(-10, +10),
+                ylim = c(0,100),
+                pCutoff = 0.05,
+                FCcutoff = 2, 
+                title = "del_17p vs. control samples"
+)
+
+
+
 
 
 
